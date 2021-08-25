85d9ad613e6ea4eb50d4e022d842d293d8d381a2
Amr abu aza <amrabuaza@gmail.com>
2021-08-25 22:02:33 +0300

    Fix

--- lib/storybook_engine/engine.rb
+++ lib/storybook_engine/engine.rb
@@ -1,5 +1,6 @@
 module StorybookEngine
   class Engine < ::Rails::Engine
     require "view_component/storybook/engine"
+    Engine.config.paths.add "app/components", eager_load: true, glob: "*"
   end
 end
