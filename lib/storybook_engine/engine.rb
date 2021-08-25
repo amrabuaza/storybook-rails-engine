b8fed431de8c4b0317e5a75cf10b601e46cc2d50
Amr abu aza <amrabuaza@Amrs-MacBook-Pro.local>
2021-08-25 11:28:37 +0300

    update

--- lib/storybook_engine/engine.rb
+++ lib/storybook_engine/engine.rb
@@ -1,8 +1,5 @@
 module StorybookEngine
   class Engine < ::Rails::Engine
     require "view_component/storybook/engine"
-    initializer "static assets" do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-    end
   end
 end
